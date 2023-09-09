586ab8aafaeede41ac26104fdaee6906e51057c2
a8chakra <anurag.chakraborty98@gmail.com>
2023-09-09 18:54:21 -0400

    push

--- src/processor/operator/recursive_extend/recursive_join.cpp
+++ src/processor/operator/recursive_extend/recursive_join.cpp
@@ -151,13 +151,13 @@ bool RecursiveJoin::getNextTuplesInternal(ExecutionContext* context) {
     // if statement checks if we are in the outputting phase and if so, scans a vector to output and
     // returns true. Otherwise, we compute a new BFS.
     while (true) {
-        if (scanOutput()) { // Phase 2
-            return true;
-        }
         if (!computeBFS(context)) {
             return false;
         }
         frontiersScanner->resetState(*bfsMorsel);
+        if (scanOutput()) { // Phase 2
+            return true;
+        }
     }
 }
 
